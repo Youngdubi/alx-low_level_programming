5a131d1584db730897257c09f925067e4af65608
Youngdubi <eberenwosuc@gmail.com>
2022-10-16 13:45:33 -0700

    poy

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%dis positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n )
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
